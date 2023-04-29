77044987918099c1ec77ae9381a8a93db5e63721
olgakahraman <ol.khrmn@gmail.com>
2023-04-29 15:32:52 -0400

    used createElement() and append() instead of innerHTML

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 
-
-inputBtn.addEventListener("click", function(){
+inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-    console.log(myLeads);
+  console.log(myLeads);
+});
+for (let i = 0; i < myLeads.length; i++) {
+  //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
 
-})
-for (let i = 0; i < myLeads.length; i++ ) {
-   ulEl.innerHTML += "<li>" + myLeads[i] +"</li>";
+  // create element
+  //set text content
+  //append to ul
+
+  const li = document.createElememnt("li");
+  li.textContent = myLeads[i]
+  ulEl.append(li);
 }
-
