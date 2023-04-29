3098d5be48c352fd810836189002800d19bbdc78
olgakahraman <ol.khrmn@gmail.com>
2023-04-29 17:21:21 -0400

    created the render function

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-let myLeads = ["olga", "eyup", "filiz"];
+let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  renderLeads();
 });
-for (let i = 0; i < myLeads.length; i++) {
-  //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
 
-  // create element
-  //set text content
-  //append to ul
+function renderLeads() {
+  let listItems = "";
 
-  const li = document.createElememnt("li");
-  li.textContent = myLeads[i]
-  ulEl.append(li);
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+    console.log(listItems);
+    // create element
+    //set text content
+    //append to ul
+
+    //const li = document.createElememnt("li");
+    //li.textContent = myLeads[i]
+    //ulEl.append(li);
+  }
+  ulEl.innerHTML = listItems;
 }
