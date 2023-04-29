4b4a93531d451fa559c36331ae41a220edcb1a8d
olgakahraman <ol.khrmn@gmail.com>
2023-04-29 00:04:36 -0400

    rendered <li> elements with the innerHTML

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let myLeads = ["olga", "eyup", "filiz"];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value);
-    console.log(myLeads);
-})
+const ulEl = document.querySelector("#ul-el");
 
+
+inputBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value);
+    console.log(myLeads);
+
+})
 for (let i = 0; i < myLeads.length; i++ ) {
-    console.log(myLeads[i]);
+   ulEl.innerHTML += "<li>" + myLeads[i] +"</li>";
 }
+
