1c5b7e0af12d883a1b82fadcdec964862cfe3a79
olgakahraman <ol.khrmn@gmail.com>
2023-04-28 23:13:33 -0400

    push the value from input field

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 inputBtn.addEventListener("click", function(){
-    myLeads.push("www.awesomelead.com");
+    myLeads.push(inputEl.value);
     console.log(myLeads);
 })
 
