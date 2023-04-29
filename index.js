b073f704229b9e297093c9bcef2878763f5e1440
olgakahraman <ol.khrmn@gmail.com>
2023-04-28 22:12:32 -0400

    refactor to addEventListener

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function saveLead(){
-    console.log("Button clicked!");
-}
+let inputBtn = document.querySelector("#input-btn");
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from addEventListener")
+})
