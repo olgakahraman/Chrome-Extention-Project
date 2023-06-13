2ab4844e7b396986c874136c81a80696cc4b883b
olgakahraman <ol.khrmn@gmail.com>
2023-06-13 15:01:31 -0400

    made the delete butto work

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+const deleteBtn = document.getElementById("delete-btn");
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 console.log(leadsFromLocalStorage);
 
 
@@ -12,6 +13,13 @@ if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+ 
+  localStorage.clear()
+ myLeads = [];
+ renderLeads();
+})
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
